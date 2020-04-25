0c34bf72378e0e7df514f68d904924beb2abf72d
Danisovszky Márk <dmarkprog@gmail.com>
2020-04-25 13:52:54 +0200

    Enemy abstract class megírása
    Megírtam az Enemy abstract classt mely összefoglalja az összes ellenfélre vonatkozó alap információkat. Továbbá elkészítettem a MossGiant és Spider osztályokat is, melyek öröklik az Enemy osztályt.

---
+++ Dungeon Escape/Assets/Scripts/Enemy/MossGiant.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossGiant : Enemy
+{
+    private void Start()
+    {
+        this.Attack();
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+        Debug.Log("MossGiantAttack called.");
+    }
+
+    public override void Update()
+    {
+        Debug.Log("MossGiantUpdate called.");
+    }
+}
---
+++ Dungeon Escape/Assets/Scripts/Enemy/Spider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : Enemy
+{
+    private void Start()
+    {
+        this.Attack();
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+        Debug.Log("SpiderAttack called.");
+    }
+
+    public override void Update()
+    {
+        Debug.Log("SpiderUpdate called.");
+    }
+}
